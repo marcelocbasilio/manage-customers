3df303ff8032c2e15c7c47e1340a0b43bd86d1fd
Marcelo Basílio <marcelocbasilio@gmail.com>
2023-12-28 15:53:52 -0300

    [fourth] Creating the DTO client.

---
+++ src/main/java/br/com/marcelocbasilio/managecustomers/dto/ClientDTO.java
@@ -0,0 +1,64 @@
+package br.com.marcelocbasilio.managecustomers.dto;
+
+import br.com.marcelocbasilio.managecustomers.entities.Client;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+
+    private Long id;
+    @NotBlank(message = "Campo requerido")
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres.")
+    private String name;
+    private String cpf;
+    private Double income;
+    @NotBlank(message = "Campo requerido")
+    @PastOrPresent(message = "Não pode fornecer datas futuras.")
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDTO(Client client) {
+        this.id = client.getId();
+        this.name = client.getName();
+        this.cpf = client.getCpf();
+        this.income = client.getIncome();
+        this.birthDate = client.getBirthDate();
+        this.children = client.getChildren();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+}
