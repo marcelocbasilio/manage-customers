43cdf47c613f7fe9abf5e767ed7a86d30f9d4ad0
Marcelo Basílio <marcelocbasilio@gmail.com>
2023-12-28 16:24:25 -0300

    [sixth] Creating the client controller.

---
+++ src/main/java/br/com/marcelocbasilio/managecustomers/controllers/ClientController.java
@@ -0,0 +1,24 @@
+package br.com.marcelocbasilio.managecustomers.controllers;
+
+import br.com.marcelocbasilio.managecustomers.dto.ClientDTO;
+import br.com.marcelocbasilio.managecustomers.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+}
